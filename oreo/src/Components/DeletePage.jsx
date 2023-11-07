a22c294c4a6808db616f613c74b8b325517d5c62
Pranav_J <107476035+pranav-jayaprakash@users.noreply.github.com>
2023-11-07 19:53:09 +0530

    Update DeletePage.jsx

--- oreo/src/Components/DeletePage.jsx
+++ oreo/src/Components/DeletePage.jsx
@@ -7,7 +7,7 @@ function DeleteContext() {
   const dispatch = useDispatch();
 
   var Currentdata = useSelector((state) => state.Userdata.loginData);
-  console.log("current dataz", Currentdata);
+  console.log("current data", Currentdata);
 
   const iddata = Currentdata[0] && Currentdata[0]._id;
 
